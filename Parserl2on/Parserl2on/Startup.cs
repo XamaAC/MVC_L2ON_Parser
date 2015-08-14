155ebb97a85fcf757a96831d5ea05b0d148c11e3
Alexandr Budanov <xama88@gmail.com>
2015-08-14 20:30:30 +0300

    ololo

---
+++ Parserl2on/Parserl2on/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Parserl2on.Startup))]
+namespace Parserl2on
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
